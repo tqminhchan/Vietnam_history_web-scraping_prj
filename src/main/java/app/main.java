e3c40b5e2d13d8e19d4cf5a3e88cc9fcdb9c0b20
Quang Minh <89254600+tqminhchan@users.noreply.github.com>
2023-05-23 17:13:15 +0700

    Add files via upload

---
+++ src/main/java/app/main.java
@@ -0,0 +1,11 @@
+package app;
+
+import crawler.CrawlerManager;
+
+import java.io.IOException;
+
+public class main {
+    public static void main(String[] args) throws IOException {
+        CrawlerManager.getData();
+    }
+}
